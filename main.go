9be517a5b5ce159b83c21f5d1b5f2a6078238b81
Tom B <53711814+tbal999@users.noreply.github.com>
2020-06-14 13:17:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"gameoflife/lifeform"
+	"image"
+	"log"
 	"math/rand"
 	"os"
 	"time"
+
+	ui "github.com/gizak/termui/v3"
 )
 
 //Initialise world - a 30x80 array of lifeforms
-var world = lifeform.Newworld(30, 80)
+var world = lifeform.Newworld(200, 100)
 var cycle = 0
+var c = ui.NewCanvas()
+var step time.Duration = 50
+var interval = step * time.Millisecond
 
 //Prints out the array - '.' is the lifeform is alive and blank means the lifeform is dead.
 func frame(i [][]lifeform.Lifeform) {
@@ -36,6 +43,26 @@ func frame(i [][]lifeform.Lifeform) {
 	fmt.Printf("Cycle: %d \n", cycle)
 }
 
+//Prints out the array using termui as a GUI
+func guiframe(i [][]lifeform.Lifeform) {
+	for y := range i {
+		for x := range i[y] {
+			if i[y][x].Alive == 0 {
+				c.SetPoint(image.Pt(y, x), 0)
+			} else {
+				c.SetPoint(image.Pt(y, x), 2)
+			}
+			if i[y][x].Next == 0 {
+				i[y][x].Alive = 0
+			} else {
+				i[y][x].Alive = 1
+			}
+		}
+	}
+	ui.Render(c)
+	fmt.Printf("Cycle: %d\r", cycle)
+}
+
 //Generates a random number
 func randomNumber(min, max int) int {
 	z := rand.Intn(max)
@@ -45,7 +72,7 @@ func randomNumber(min, max int) int {
 	return z
 }
 
-//Generates a 1 or a 0
+//Generates a random number
 func oneOrzero() int {
 	z := rand.Intn(2)
 	if z < 1 {
@@ -59,9 +86,9 @@ func oneOrzero() int {
 //Seeds the map with new alive lifeforms randomly
 func seed(w *[][]lifeform.Lifeform) {
 	world := *w
-	for i := 0; i < 1200; i++ {
-		y := randomNumber(5, 25)
-		x := randomNumber(5, 70)
+	for i := 0; i < 5000; i++ {
+		y := randomNumber(5, 180)
+		x := randomNumber(5, 90)
 		world[y][x].Alive = oneOrzero()
 		world[y][x].Next = oneOrzero()
 	}
@@ -83,8 +110,9 @@ func reset(world *[][]lifeform.Lifeform) {
 //Main entry
 func main() {
 	Scanner := bufio.NewScanner(os.Stdin)
+Start:
 	game := true
-	fmt.Println("Game of Life - press 'n' for new generated world, 'q' to quit or press enter to run 1 cycle")
+	fmt.Println("Game of Life - press 'n' for new generated world, 'q' to quit, 'g' for GUI low res version or press enter to run 1 cycle")
 	for game == true {
 		Scanner.Scan()
 		result := Scanner.Text()
@@ -96,6 +124,36 @@ func main() {
 			cycle = 0
 		case "q":
 			game = false
+		case "g":
+			fmt.Println("Entering GUI version - press 'q' at any time to return to menu, or 'n' to refresh GUI version")
+			Scanner.Scan()
+			rand.Seed(time.Now().UTC().UnixNano())
+			seed(&world)
+			c.SetRect(0, 0, 200, 100)
+			if err := ui.Init(); err != nil {
+				log.Fatalf("failed to initialize termui: %v", err)
+			}
+			uiEvents := ui.PollEvents()
+			ticker := time.NewTicker(interval).C
+			for {
+				select {
+				case e := <-uiEvents:
+					switch e.ID { // event string/identifier
+					case "q", "<C-c>": // press 'q' or 'C-c' to quit
+						ui.Close()
+						goto Start
+					case "n":
+						cycle = 0
+						reset(&world)
+						seed(&world)
+					}
+				// use Go's built-in tickers for updating and drawing data
+				case <-ticker:
+					cycle++
+					lifeform.Adjust(&world)
+					guiframe(world)
+				}
+			}
 		default:
 			lifeform.Adjust(&world)
 			frame(world)
